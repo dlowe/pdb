5e614ed0d384e13644df2959c7f526d36f3df954
dlowe <none@none>
2007-11-06 14:00:53 -0800

    tests

--- test/0/smoke.t
+++ test/0/smoke.t
@@ -6,28 +6,27 @@ use warnings;
 use Test;
 use Socket;
 
-BEGIN { plan tests => 6 }
+BEGIN { plan tests => 5 }
+
+require('test/PDBTest.pm');
 
 my $pdb = "./pdb";
-ok(-x $pdb, 1, "can't find pdb") or die;
+ok(PDBTest::exists(), 1, "can't find pdb") or die;
 
-my $output = `$pdb 2>&1`;
-ok($output, '', "pdb failed to boot: $output") or die;
+my $pid;
+eval {
+    $pid = PDBTest::startup();
+};
+ok($@, '', "pdb failed to boot: $@") or die;
 
-my $pid = `ps -ax | grep $pdb | grep -v grep | awk '{print \$1}'`;
-chomp $pid;
 ok($pid, qr/^\d+$/, "can't figure out pdb process id");
 
 socket(SH, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
 connect(SH, sockaddr_in(5032, inet_aton("localhost")));
 ok(<SH>, "poot!\n", 'pdb not answering');
+close(SH);
 
-$output = `kill $pid`;
-ok($output, '', "unable to kill $pid: $output");
-
-# admittedly hacky
-select(undef, undef, undef, 0.250);
-
-$pid = `ps -ax | grep $pdb | grep -v grep | awk '{print \$1}'`;
-chomp $pid;
-ok($pid, '', "pdb didn't die: $pdb");
+eval {
+    PDBTest::shutdown();
+};
+ok($@, '', "shutdown failed: $@");
--- test/PDBTest.pm
+++ test/PDBTest.pm
@@ -5,7 +5,11 @@ use warnings;
 
 package PDBTest;
 
-our $pid = "./pdb";
+our $pdb = "./pdb";
+
+sub exists {
+    return -x $pdb;
+}
 
 sub startup {
     my $output = `$pdb 2>&1`;
@@ -25,29 +29,7 @@ sub shutdown {
     die $output if $output ne '';
     select(undef, undef, undef, 0.250);
     $pid = pid();
-    die if $pid ne '';
+    die "pdb didn't die: $pid" if $pid ne '';
 }
 
-my $pdb = "./pdb";
-ok(-x $pdb, 1, "can't find pdb") or die;
-
-my $output = `$pdb 2>&1`;
-ok($output, '', "pdb failed to boot: $output") or die;
-
-my $pid = `ps -ax | grep $pdb | grep -v grep | awk '{print \$1}'`;
-chomp $pid;
-ok($pid, qr/^\d+$/, "can't figure out pdb process id");
-
-socket(SH, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
-connect(SH, sockaddr_in(5032, inet_aton("localhost")));
-ok(<SH>, "poot!\n", 'pdb not answering');
-
-$output = `kill $pid`;
-ok($output, '', "unable to kill $pid: $output");
-
-# admittedly hacky
-select(undef, undef, undef, 0.250);
-
-$pid = `ps -ax | grep $pdb | grep -v grep | awk '{print \$1}'`;
-chomp $pid;
-ok($pid, '', "pdb didn't die: $pdb");
+1;
