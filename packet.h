ef75b05f1566bc04b021aa2e14df3a89da2128ab
dlowe <none@none>
2007-11-25 20:33:15 -0800

    docs

--- packet.h
+++ packet.h
@@ -45,12 +45,50 @@ typedef packet_status (*packet_writer)(int, packet *, int *);
  */
 packet *packet_NULL(void);
 
+/**
+ * Allocate a new packet object.
+ *
+ * @return freshly allocated packet, initialized to empty
+ */
 packet *packet_new();
+
+/**
+ * Create a copy of a packet.
+ *
+ * @param[in] p original packet
+ * @return freshly allocated packet, initialized to be identical to original
+ */
 packet *packet_copy(packet *p);
+
+/**
+ * Delete a packet
+ *
+ * @param[in,out] p a packet
+ */
 void packet_delete(packet *p);
 
+/**
+ * Allocate a new packet set.
+ *
+ * @param[in] count the number of packets to be contained in the set.
+ * @return freshly allocated packet set of 'count' empty packets
+ */
 packet_set *packet_set_new(int count);
+
+/**
+ * Fetch a packet out of a set.
+ *
+ * @param[in] p a packet set
+ * @param[in] index the index of the packet to fetch
+ * @return pointer to the packet
+ */
 packet *packet_set_get(packet_set *p, int index);
+
+/**
+ * Delete a packet set (includes deleting all contained packets!)
+ *
+ * @param[in,out] p a packet set
+ */
 void packet_set_delete(packet_set *p);
 
 #endif
